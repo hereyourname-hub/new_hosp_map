57d582ac7ca58e644b579917a74ae310d91ee62a
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-06-24 07:57:24 +0300

    end1

--- map/views.py
+++ map/views.py
@@ -4,27 +4,37 @@ from .models import Location
 from folium.plugins import FastMarkerCluster
 
 
+from django.shortcuts import render
+import folium
+from .models import Location
+from folium.plugins import MarkerCluster
+
+
+from django.shortcuts import render
+import folium
+from .models import Location
+from folium.plugins import MarkerCluster
+
+
+from django.shortcuts import render
+import folium
+from .models import Location
+from folium.plugins import MarkerCluster
+
+
 def home(request):
     locations = Location.objects.all()
 
     initialMap = folium.Map(location=[40.8218488, 73.581502], zoom_start=7)
-    latitudes = []
-    longitudes = []
+    marker_cluster = MarkerCluster().add_to(initialMap)
 
-    # нахождение долготы и широты
     for location in locations:
-        latitudes.append(location.lat)
-        longitudes.append(location.lng)
-
-    print(latitudes)
-    print(longitudes)
-    print(list(zip(latitudes, longitudes)))
-
-    FastMarkerCluster(data=zip(latitudes, longitudes)).add_to(initialMap)
-
-    # for location in locations:
-    #     coordinates = (location.lat, location.lng)
-    #     folium.Marker(coordinates, popup='Наименование: ' + location.name).add_to(initialMap)
+        coordinates = (location.lat, location.lng)
+        popup_content = f"<strong><span style='font-size: 14px;'>Наименование:</span></strong> {location.name}"
+        icon = folium.Icon(icon='hospital', prefix='fa', color='red')
+        folium.Marker(coordinates, popup=popup_content, icon=icon).add_to(marker_cluster)
 
     context = {'map': initialMap._repr_html_(), 'locations': locations}
     return render(request, 'map/home.html', context)
+
+
